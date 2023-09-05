70d980ac6f99684da5893e5de6c476ea6e1ee506
Luis Pinilla <123709767+LuigiEnLaCasa@users.noreply.github.com>
2023-09-04 19:32:25 -0500

    Add files via upload

---
+++ Taller 2 - Hamburguesas_esqueleto/Hamburguesas/src/Logic/ProductoMenu.java
@@ -0,0 +1,45 @@
+package Logic;
+
+public class ProductoMenu implements Producto {
+	
+	
+	//*** Atributos Clase menú ***//
+	
+	private String nombre ;
+	private int precioBase;
+	
+	
+	//*** Métodos Calse producto menú ***//
+	
+	
+	public ProductoMenu(String nombre, int precioBase) {
+		
+		this.nombre = nombre;
+		this.precioBase = precioBase;
+		
+	}
+
+
+	@Override
+	public int getPrecio() {
+		// TODO Auto-generated method stub
+		return this.precioBase;
+	}
+
+
+	@Override
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return this.nombre;
+	}
+
+
+	@Override
+	public String generarTextoFactura() {
+		// TODO Auto-generated method stub
+		return null;
+	};
+
+	
+	
+}
